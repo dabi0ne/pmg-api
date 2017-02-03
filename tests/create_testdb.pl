891e1a24fef08d34e86c12a875e233273e8d330f
Dietmar Maurer <dietmar@proxmox.com>
2017-02-03 10:08:13 +0100

    add some regression tests

---
+++ tests/create_testdb.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+use DBI;
+
+use PVE::SafeSyslog;
+
+use PMG::DBTools;
+use PMG::RuleDB;
+
+initlog ($0, 'mail');
+
+my $list = PMG::DBTools::database_list();
+
+my $dbname = "Proxmox_testdb";
+
+if ($list->{$dbname}) {
+
+    print "delete existing test database\n";
+    PMG::DBTools::delete_ruledb($dbname);
+}
+
+my $dbh = PMG::DBTools::create_ruledb($dbname);
+my $ruledb = PMG::RuleDB->new($dbh);
+
+$ruledb->close();
+
+exit (0);
