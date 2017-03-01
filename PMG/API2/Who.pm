f2becee692d023c730a9dda6f964cab37916f7d5
Dietmar Maurer <dietmar@proxmox.com>
2017-03-01 11:10:57 +0100

    allow to set object group properties

--- PMG/API2/Who.pm
+++ PMG/API2/Who.pm
@@ -50,6 +50,7 @@ __PACKAGE__->register_method ({
 	my ($param) = @_;
 
 	return [
+	    { subdir => 'config' },
 	    { subdir => 'objects' },
 	    { subdir => 'email' },
 	    { subdir => 'domain' },
@@ -87,6 +88,95 @@ __PACKAGE__->register_method({
 
 	return undef;
     }});
+
+__PACKAGE__->register_method ({
+    name => 'get_config',
+    path => 'config',
+    method => 'GET',
+    description => "Get who group properties",
+    proxyto => 'master',
+    parameters => {
+	additionalProperties => 0,
+	properties => {
+	    ogroup => {
+		description => "Object Group ID.",
+		type => 'integer',
+	    },
+	},
+    },
+    returns => {
+	type => "object",
+	properties => {
+	    id => { type => 'integer'},
+	    name => { type => 'string' },
+	    info => { type => 'string' },
+	},
+    },
+    code => sub {
+	my ($param) = @_;
+
+	my $rdb = PMG::RuleDB->new();
+
+	my $list = $rdb->load_objectgroups('who', $param->{ogroup});
+	my $og = shift @$list ||
+	    die "who group '$param->{ogroup}' not found\n";
+
+	return {
+	    id => $og->{id},
+	    name => $og->{name},
+	    info => $og->{info},
+	};
+   }});
+
+__PACKAGE__->register_method ({
+    name => 'set_config',
+    path => 'config',
+    method => 'PUT',
+    description => "Modify who group properties",
+    proxyto => 'master',
+    parameters => {
+	additionalProperties => 0,
+	properties => {
+	    ogroup => {
+		description => "Object Group ID.",
+		type => 'integer',
+	    },
+	    name => {
+		description => "Group name.",
+		type => 'string',
+		maxLength => 255,
+		optional => 1,
+	    },
+	    info => {
+		description => "Informational comment.",
+		type => 'string',
+		maxLength => 255,
+		optional => 1,
+	    },
+	},
+    },
+    returns => { type => "null" },
+    code => sub {
+	my ($param) = @_;
+
+	my $rdb = PMG::RuleDB->new();
+
+	my $ogroup = extract_param($param, 'ogroup');
+
+	die "no options specified\n"
+	    if !scalar(keys %$param);
+
+	my $list = $rdb->load_objectgroups('who', $ogroup);
+	my $og = shift @$list ||
+	    die "who group '$ogroup' not found\n";
+
+	$og->{name} = $param->{name} if defined($param->{name});
+	$og->{info} = $param->{info} if defined($param->{info});
+
+	$rdb->save_group($og);
+
+	return undef;
+  }});
 
 __PACKAGE__->register_method ({
     name => 'objects',
