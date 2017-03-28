277c84e453ef6e223bf4feb0047d698fc3805b11
Dietmar Maurer <dietmar@proxmox.com>
2017-03-28 10:03:10 +0200

    PMG/API2/Users.pm: use correct format for userid

--- PMG/API2/Users.pm
+++ PMG/API2/Users.pm
@@ -173,7 +173,7 @@ __PACKAGE__->register_method ({
     parameters => {
 	additionalProperties => 0,
 	properties => {
-	    userid => get_standard_option('username'),
+	    userid => get_standard_option('userid'),
 	}
     },
     returns => { type => 'null' },
