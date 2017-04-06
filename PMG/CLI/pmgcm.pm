3879a8aa35d7e8130ad1c96b8f4f7aa848f8f0c3
Dietmar Maurer <dietmar@proxmox.com>
2017-04-06 09:56:31 +0200

    pmgcm join_cmd: new helper

--- PMG/CLI/pmgcm.pm
+++ PMG/CLI/pmgcm.pm
@@ -27,17 +27,47 @@ my $format_nodelist = sub {
     print "NAME(CID)--------------IPADDRESS----ROLE-STATE---------UPTIME---LOAD----MEM---DISK\n";
     foreach my $ni (@$res) {
 	my $state = '?';
-	
-	printf "%-20s %-15s %-6s %1s %15s %6s %5s%% %5s%%\n", 
-	"$ni->{name}($ni->{cid})", $ni->{ip}, $ni->{type}, 
+
+	printf "%-20s %-15s %-6s %1s %15s %6s %5s%% %5s%%\n",
+	"$ni->{name}($ni->{cid})", $ni->{ip}, $ni->{type},
 	$state, '-', '-', '-', '-';
     }
 };
+
+__PACKAGE__->register_method({
+    name => 'join_cmd',
+    path => 'join_cmd',
+    method => 'GET',
+    description => "Print information about cluster master node.",
+    parameters => {
+	additionalProperties => 0,
+	properties => {},
+    },
+    returns => { type => 'null' },
+    code => sub {
+	my ($param) = @_;
+
+	my $cfg = PVE::INotify::read_file('cluster.conf');
+
+	if (scalar(keys %{$cfg->{ids}})) {
+
+	    my $master = $cfg->{master} ||
+		die "no master found\n";
+
+	    print "pmgcm join --master_ip $master->{ip} --fingerprint $master->{fingerprint}\n";
+
+	} else {
+	    die "no cluster defined\n";
+	}
+
+	return undef;
+    }});
 
 our $cmddef = {
     nodes => [ 'PMG::API2::Cluster', 'nodes', [], {}, $format_nodelist],
     create => [ 'PMG::API2::Cluster', 'create', []],
     join => [ 'PMG::API2::Cluster', 'join', ['master_ip', 'fingerprint']],
+    join_cmd => [ __PACKAGE__, 'join_cmd', []],
 };
 
 1;
