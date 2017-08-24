91bc765239b5b12c694af39c0acb55fb2915defe
Dietmar Maurer <dietmar@proxmox.com>
2017-08-24 10:28:13 +0200

    add virus and spam statistic API

--- PMG/API2/Statistics.pm
+++ PMG/API2/Statistics.pm
@@ -67,17 +67,127 @@ __PACKAGE__->register_method ({
     },
     code => sub {
 	my ($param) = @_;
-	
+
 	my $restenv = PMG::RESTEnvironment->get();
 	my $cinfo = $restenv->{cinfo};
 
 	my $start = $param->{starttime} // (time - 86400);
 	my $end = $param->{endtime} // ($start + 86400);
-	
+
 	my $stat = PMG::Statistic->new($start, $end);
 	my $rdb = PMG::RuleDB->new();
-	
+
 	my $res = $stat->total_mail_stat($rdb);
+
+	return $res;
+    }});
+
+__PACKAGE__->register_method ({
+    name => 'virus',
+    path => 'virus',
+    method => 'GET',
+    description => "Get Statistics about detected Viruses.",
+    permissions => { check => [ 'admin', 'qmanager', 'audit'] },
+    proxyto => 'node',
+    parameters => {
+	additionalProperties => 0,
+	properties => {
+	    node => get_standard_option('pve-node'),
+	    starttime => get_standard_option('pmg-starttime'),
+	    endtime => get_standard_option('pmg-endtime'),
+	},
+    },
+    returns => {
+	type => 'array',
+	items => {
+	    type => "object",
+	    properties => {
+		name => {
+		    description => 'Virus name.',
+		    type => 'string',
+		},
+		count => {
+		    description => 'Detection count.',
+		    type => 'integer',
+		},
+	    },
+	}
+    },
+    code => sub {
+	my ($param) = @_;
+
+	my $restenv = PMG::RESTEnvironment->get();
+	my $cinfo = $restenv->{cinfo};
+
+	my $start = $param->{starttime} // (time - 86400);
+	my $end = $param->{endtime} // ($start + 86400);
+
+	my $stat = PMG::Statistic->new($start, $end);
+	my $rdb = PMG::RuleDB->new();
+
+	my $res = $stat->total_virus_stat($rdb);
+
+	return $res;
+    }});
+
+__PACKAGE__->register_method ({
+    name => 'spam',
+    path => 'spam',
+    method => 'GET',
+    description => "Get Statistics about detected Spam.",
+    permissions => { check => [ 'admin', 'qmanager', 'audit'] },
+    proxyto => 'node',
+    parameters => {
+	additionalProperties => 0,
+	properties => {
+	    node => get_standard_option('pve-node'),
+	    starttime => get_standard_option('pmg-starttime'),
+	    endtime => get_standard_option('pmg-endtime'),
+	},
+    },
+    returns => {
+	type => 'array',
+	items => {
+	    type => "object",
+	    properties => {
+		level => {
+		    description => 'Spam level.',
+		    type => 'string',
+		},
+		count => {
+		    description => 'Detection count.',
+		    type => 'integer',
+		},
+	    },
+	}
+    },
+    code => sub {
+	my ($param) = @_;
+
+	my $restenv = PMG::RESTEnvironment->get();
+	my $cinfo = $restenv->{cinfo};
+
+	my $start = $param->{starttime} // (time - 86400);
+	my $end = $param->{endtime} // ($start + 86400);
+
+	my $stat = PMG::Statistic->new($start, $end);
+	my $rdb = PMG::RuleDB->new();
+
+	my $totalstat = $stat->total_mail_stat ($rdb);
+	my $spamstat = $stat->total_spam_stat($rdb);
+
+	my $res => [];
+
+	my $rest = $totalstat->{spamcount_in};
+	foreach my $ref (@$spamstat) {
+	    next if $ref->{spamlevel} >= 10;
+	    next if $ref->{spamlevel} < 1;
+	    $rest -= $ref->{count} if $ref->{spamlevel} >= 3;
+	    push @$res, { level =>  $ref->{spamlevel}, count => $ref->{count} };
+	}
+
+	push @$res, { level => 10, count => $rest } if $rest;
+	push @$res, { level => 0, count => $totalstat->{count_in} - $totalstat->{spamcount_in}};
 
 	return $res;
     }});
